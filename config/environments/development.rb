a874cbad46222d58c0ccf6cd329fd37df8c02400
Pascal Wever <pwever@knittingpixel.com>
2011-01-27 09:52:43 -0800

    mailer update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,5 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+config.action_controller.session[:session_domain] = "EchoPark.local" # or whatever
